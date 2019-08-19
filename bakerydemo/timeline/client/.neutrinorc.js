75a0c283b0949c7c763b9922ff788e811b75677f
LB Johnston <mail@lb.ee>
2019-08-20 08:46:59 +1000

    Attempt to configure to not bring in 'require' by default

--- bakerydemo/timeline/client/.neutrinorc.js
+++ bakerydemo/timeline/client/.neutrinorc.js
@@ -1,7 +1,47 @@
 module.exports = {
   use: [
-    '@neutrinojs/airbnb',
-    '@neutrinojs/react-components',
-    '@neutrinojs/jest'
+    "@neutrinojs/airbnb",
+    ["@neutrinojs/react-components", { mains: 'fff'}]
+    // [
+    //   "@neutrinojs/react-components",
+    //   {
+    //     // babel: {
+    //     //   // Override options for babel-preset-env:
+    //     //   presets: [
+    //     //     [
+    //     //       "babel-preset-env",
+    //     //       {
+    //     //         modules: false,
+    //     //         useBuiltIns: true,
+    //     //         exclude: [
+    //     //           "transform-regenerator",
+    //     //           "transform-async-to-generator"
+    //     //         ]
+    //     //       }
+    //     //     ]
+    //     //   ]
+    //     // }
+    //     babel: {
+    //       presets: [],
+    //     },
+    //     options: {
+    //       babel: {
+    //         presets: [],
+    //       },
+    //       // target: 'web'
+    //       externals: {
+    //         'BAZ': "BAR"
+    //     //     react: {
+    //     //       // commonjs: 'lodash',
+    //     //       // amd: 'lodash',
+    //     //       root: "_" // indicates global variable
+    //     //     }
+    //       }
+    //     }
+    //   }
+    // ],
+    // "@neutrinojs/jest",
+    // function(options) {return {}}
+    // ["@neutrinojs/library", { name: 'Logger' }]
   ]
 };
