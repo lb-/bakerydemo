1aa28ee7232b35d35626c75047638545ec7fa2fb
LB Johnston <mail@lb.ee>
2019-08-24 11:52:24 +1000

    follow basic react-calendar-timeline getting started to render calendar

--- bakerydemo/timeline/client/src/components/HelloWorld/index.jsx
+++ bakerydemo/timeline/client/src/components/HelloWorld/index.jsx
@@ -1,5 +1,9 @@
 import { PureComponent } from 'react'; // eslint-disable-line import/no-extraneous-dependencies
 import { string } from 'prop-types'; // eslint-disable-line import/no-extraneous-dependencies
+import Timeline from 'react-calendar-timeline';
+// make sure you include the timeline stylesheet or the timeline will not be styled
+import 'react-calendar-timeline/lib/Timeline.css';
+import moment from 'moment';
 
 import './style.css';
 
@@ -28,6 +32,32 @@ export default class HelloWorld extends PureComponent {
     }
   }
 
+  getGroups = () => [{ id: 1, title: 'group 1' }, { id: 2, title: 'group 2' }];
+
+  getItems = () => [
+    {
+      id: 1,
+      group: 1,
+      title: 'item 1',
+      start_time: moment(),
+      end_time: moment().add(1, 'hour'),
+    },
+    {
+      id: 2,
+      group: 2,
+      title: 'item 2',
+      start_time: moment().add(-0.5, 'hour'),
+      end_time: moment().add(0.5, 'hour'),
+    },
+    {
+      id: 3,
+      group: 1,
+      title: 'item 3',
+      start_time: moment().add(2, 'hour'),
+      end_time: moment().add(3, 'hour'),
+    },
+  ]
+
   handleClick = () => {
     this.setState({
       color: generateColor(),
@@ -39,6 +69,12 @@ export default class HelloWorld extends PureComponent {
       <div className="hello">
         <h1 style={{ color: this.state.color, padding: '20px' }}>Hello World!</h1>
         <button onClick={this.handleClick}>Change color</button>
+        <Timeline
+          groups={this.getGroups()}
+          items={this.getItems()}
+          defaultTimeStart={moment().add(-12, 'hour')}
+          defaultTimeEnd={moment().add(12, 'hour')}
+        />
       </div>
     );
   }
