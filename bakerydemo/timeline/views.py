3328a18e8b5a30490a58b6c963084925ca921d66
LB Johnston <mail@lb.ee>
2019-08-26 08:14:43 +1000

    Timeline view - use wagtail admin header with search field

--- bakerydemo/timeline/views.py
+++ bakerydemo/timeline/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from wagtail.admin.forms.search import SearchForm
+
 
 def timeline_view(request):
 
-    return render(request, "timeline.html", {})
-
+    return render(request, "timeline.html", {
+        'icon': 'time',
+        'search_form': SearchForm(),
+        'search_url': 'timeline',  # url name set by wagtail_hooks
+        'title': 'Timeline'
+    })
