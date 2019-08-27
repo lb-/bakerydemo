6895bb7a4eb29f3bf582ee7c228ae1cb897e6603
LB Johnston <mail@lb.ee>
2019-08-28 09:29:37 +1000

    Demo js - format file & add prop to Example

--- bakerydemo/timeline/client/demo/src/index.js
+++ bakerydemo/timeline/client/demo/src/index.js
@@ -1,15 +1,21 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-import Example from '../../src'
+import Example from '../../src';
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>client Demo</h1>
-      <Example/>
-    </div>
+    return (
+      <div>
+        <h1>client Demo</h1>
+        <form action="/admin/timeline/" method="get" role="search">
+          <label htmlFor="id_q">Search term:</label>
+          <input type="text" name="q" id="id_q" placeholder="Search" />
+        </form>
+        <Example searchFormId="id_q" />
+      </div>
+    );
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'));
