fc848f6726945bc4cd9b19304b7bb71b57cbf04d
LB Johnston <mail@lb.ee>
2019-08-28 09:29:06 +1000

    Timeline View - handle server request / search query in URL

--- bakerydemo/timeline/views.py
+++ bakerydemo/timeline/views.py
@@ -4,10 +4,19 @@ from wagtail.admin.forms.search import SearchForm
 
 
 def timeline_view(request):
+    # Search
+    query_string = None
+    if 'q' in request.GET:
+        search_form = SearchForm(request.GET, placeholder='Search timeline')
+        if search_form.is_valid():
+            query_string = search_form.cleaned_data['q']
+    else:
+        search_form = SearchForm(placeholder='Search timeline')
 
     return render(request, "timeline.html", {
         'icon': 'time',
-        'search_form': SearchForm(),
+        'query_string': query_string,
+        'search_form': search_form,
         'search_url': 'timeline',  # url name set by wagtail_hooks
-        'title': 'Timeline'
+        'title': 'Timeline',
     })
