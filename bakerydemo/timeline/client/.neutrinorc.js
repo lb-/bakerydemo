f6adc813ff077f07c5a73d0d2a6d60800ca369ef
LB Johnston <mail@lb.ee>
2019-08-29 06:59:51 +1000

    Add more notes to the neutrino config

--- bakerydemo/timeline/client/.neutrinorc.js
+++ bakerydemo/timeline/client/.neutrinorc.js
@@ -4,13 +4,16 @@ module.exports = {
     "@neutrinojs/react-components",
     "@neutrinojs/jest",
     /**
-     * by default the react-components plugin uses webpack-node-externals to build
+     * Ensure that react is read from global - and webpack-node-externals is NOT used.
+     * 
+     * By default the react-components plugin uses webpack-node-externals to build
      * the externals object. This will simply get all dependencies and assume they are
      * external AND assume that requirejs is used.
      * However, for a web usage, we want only some external dependencies set up and
      * want them to read from global (aka root), hence we map the 'react' import to 'React' global.
      * See:
      * https://github.com/neutrinojs/neutrino/issues/1425
+     * https://github.com/neutrinojs/neutrino/issues/1457
      * https://github.com/neutrinojs/neutrino/blob/master/packages/react-components/index.js#L63
      * https://www.npmjs.com/package/webpack-node-externals
      * https://webpack.js.org/configuration/externals/#externals
