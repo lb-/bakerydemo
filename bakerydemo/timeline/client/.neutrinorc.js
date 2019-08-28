17c1c18d23279b24efc90d8cd7110612d40c152b
LB Johnston <mail@lb.ee>
2019-08-28 21:18:38 +1000

    update neutrino config to allow for 'react' in global as React

--- bakerydemo/timeline/client/.neutrinorc.js
+++ bakerydemo/timeline/client/.neutrinorc.js
@@ -1,7 +1,19 @@
 module.exports = {
   use: [
-    '@neutrinojs/airbnb',
-    '@neutrinojs/react-components',
-    '@neutrinojs/jest'
+    "@neutrinojs/airbnb",
+    "@neutrinojs/react-components",
+    "@neutrinojs/jest",
+    neutrino => {
+      neutrino.config.when(process.env.NODE_ENV === "production", config => {
+        config.externals({
+          react: {
+            amd: "react",
+            commonjs: "react",
+            commonjs2: "react",
+            root: "React"
+          }
+        });
+      });
+    }
   ]
 };
