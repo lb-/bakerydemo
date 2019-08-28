f63f4064518e1df7008cb2199e690127caf366a7
LB Johnston <mail@lb.ee>
2019-08-28 10:15:57 +1000

    Experiment with including dependencies

--- bakerydemo/timeline/client/.neutrinorc.js
+++ bakerydemo/timeline/client/.neutrinorc.js
@@ -1,10 +1,31 @@
 module.exports = {
   options: {
     // output: '../../static/timeline/dist' // Override to absolute directory in django static folder
+    // externals: {
+    //   whitelist: ["moment", "react-calendar-timeline"]
+    // }
   },
   use: [
-    '@neutrinojs/airbnb',
-    '@neutrinojs/react-components',
-    '@neutrinojs/jest'
+    "@neutrinojs/airbnb",
+    [
+      "@neutrinojs/react-components",
+      {
+        // name: "HelloWorld",
+        // externals: {
+        // whitelist: ["moment", "react-calendar-timeline"]
+        // whitelist: ["react-calendar-timeline"]
+        // }
+      }
+    ],
+    "@neutrinojs/jest"
+    // [
+    //   "@neutrinojs/library",
+    //   {
+    //     name: "reduxExample",
+    //     externals: {
+    //       whitelist: ["moment", "react-calendar-timeline"]
+    //     }
+    //   }
+    // ]
   ]
 };
