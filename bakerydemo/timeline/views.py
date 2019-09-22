f81669943a2baf72f8975e956675daa29f191f62
LB <mail@lb.ee>
2019-09-22 18:57:02 +1000

    add search form and title to timeline view

--- bakerydemo/timeline/views.py
+++ bakerydemo/timeline/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
+from wagtail.admin.forms.search import SearchForm
+
 
 def timeline_view(request):
+    # Search
+    query_string = None
+    if 'q' in request.GET:
+        search_form = SearchForm(request.GET, placeholder='Search timeline')
+        if search_form.is_valid():
+            query_string = search_form.cleaned_data['q']
+    else:
+        search_form = SearchForm(placeholder='Search timeline')
 
-    return render(request, "timeline.html", {})
-
+    return render(request, "timeline.html", {
+        'icon': 'time',
+        'query_string': query_string or '',
+        'search_form': search_form,
+        'search_url': 'timeline',  # url name set by wagtail_hooks
+        'title': 'Timeline',
+    })
