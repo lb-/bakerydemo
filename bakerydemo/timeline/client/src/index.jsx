199ee22e5438c0a6f906634138cc7b3c3bef9158
LB <mail@lb.ee>
2019-09-22 18:56:44 +1000

    update demo code to add search input

--- bakerydemo/timeline/client/src/index.jsx
+++ bakerydemo/timeline/client/src/index.jsx
@@ -1,14 +1,18 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { render } from 'react-dom';
-import HelloWorld from './components/HelloWorld';
+import { render } from 'react-dom'; // eslint-disable-line import/no-extraneous-dependencies
 import Timeline from './components/Timeline';
 
 const root = document.getElementById('root');
 
 render(
-  <div>
-    <HelloWorld />
-    <Timeline />
-  </div>,
+  <main>
+    <h1>Demo</h1>
+    <form action="/admin/timeline/" method="get" role="search">
+      <label htmlFor="id_q">
+        Search term:
+        <input type="text" name="q" id="id_q" placeholder="Search" />
+      </label>
+    </form>
+    <Timeline searchFormId="id_q" />
+  </main>,
   root,
 );
