7d1b241469ad7f4e6490f77f283674b43acdefe2
LB Johnston <mail@lb.ee>
2019-11-20 07:14:16 +1000

    neutrinojs config - use React from global in production

--- bakerydemo/timeline/client/.neutrinorc.js
+++ bakerydemo/timeline/client/.neutrinorc.js
@@ -10,5 +10,26 @@ module.exports = {
     airbnb(),
     reactComponents(),
     jest(),
+    /**
+     * Ensure that react is read from global - and webpack-node-externals is NOT used.
+     *
+     * By default the react-components plugin uses webpack-node-externals to build
+     * the externals object. This will simply get all dependencies and assume they are
+     * external AND assume that requirejs is used.
+     *
+     * However, for a web usage, we want only some external dependencies set up and
+     * want them to read from global (aka root), hence we map the 'react' import to 'React' global.
+     * See:
+     * https://github.com/neutrinojs/neutrino/issues/1425
+     * https://github.com/neutrinojs/neutrino/issues/1457
+     * https://github.com/neutrinojs/neutrino/blob/master/packages/react-components/index.js#L63
+     * https://www.npmjs.com/package/webpack-node-externals
+     * https://webpack.js.org/configuration/externals/#externals
+     */
+    neutrino => {
+      neutrino.config.when(process.env.NODE_ENV === 'production', config => {
+        config.externals({ react: 'React' });
+      });
+    },
   ],
 };
