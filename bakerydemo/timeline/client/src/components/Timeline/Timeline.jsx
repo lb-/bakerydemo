e10fbd85f6d793dd52d6ebea37fe16782c131c99
LB Johnston <mail@lb.ee>
2019-12-03 08:07:00 +1000

    Timeline React component -  use API data
    - convert to class component
    - set up state
    - set up API calls and use Messages / transformer imports

--- bakerydemo/timeline/client/src/components/Timeline/Timeline.jsx
+++ bakerydemo/timeline/client/src/components/Timeline/Timeline.jsx
@@ -1,64 +1,91 @@
 // timeline/client/src/components/Timeline/Timeline.jsx
 
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import classNames from 'classnames';
-import moment from 'moment';
 import CalendarTimeline from 'react-calendar-timeline';
+
+import Messages from './Messages';
+import getTransformedResponse from './get-transformed-response';
 
 // styles
 import 'react-calendar-timeline/lib/Timeline.css'; // must include to ensure the timeline itself is styled
 import './timeline.css';
 
-const Timeline = ({ className }) => {
-  const groups = [
-    { id: 1, title: 'group 1' },
-    { id: 2, title: 'group 2' },
-  ];
+class Timeline extends PureComponent {
+  state = {
+    defaultTimes: {},
+    error: null,
+    groups: [],
+    isLoading: true,
+    items: [],
+  };
 
-  const items = [
-    {
-      id: 1,
-      group: 1,
-      title: 'item 1',
-      start_time: moment(),
-      end_time: moment().add(1, 'hour'),
-    },
-    {
-      id: 2,
-      group: 2,
-      title: 'item 2',
-      start_time: moment().add(-0.5, 'hour'),
-      end_time: moment().add(0.5, 'hour'),
-    },
-    {
-      id: 3,
-      group: 1,
-      title: 'item 3',
-      start_time: moment().add(2, 'hour'),
-      end_time: moment().add(3, 'hour'),
-    },
-  ];
+  componentDidMount() {
+    this.fetchData();
+  }
 
-  return (
-    <div className={classNames('timeline', className)}>
-      <CalendarTimeline
-        groups={groups}
-        items={items}
-        defaultTimeStart={moment().add(-12, 'hour')}
-        defaultTimeEnd={moment().add(12, 'hour')}
-      />
-    </div>
-  );
+  /** set state to loading and then call the API for the items data */
+  fetchData() {
+    const { apiUrl } = this.props;
+    this.setState({ isLoading: true });
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(({ message, ...data }) => {
+        if (message) throw new Error(message);
+        return data;
+      })
+      .then(getTransformedResponse)
+      .then(({ items, defaultTimes, groups }) =>
+        this.setState({
+          defaultTimes,
+          error: null,
+          groups,
+          isLoading: false,
+          items,
+        }),
+      )
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  render() {
+    const { className } = this.props;
+    const {
+      defaultTimes: { start, end },
+      error,
+      groups,
+      isLoading,
+      items,
+    } = this.state;
+
+    return (
+      <div className={classNames('timeline', className)}>
+        {isLoading || error ? (
+          <Messages error={error} isLoading={isLoading} />
+        ) : (
+          <CalendarTimeline
+            defaultTimeEnd={end}
+            defaultTimeStart={start}
+            groups={groups}
+            items={items}
+            sidebarWidth={250}
+            stackItems
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+Timeline.defaultProps = {
+  apiUrl: '/api/v2/pages/?limit=100',
+  className: '',
 };
 
 Timeline.propTypes = {
+  apiUrl: PropTypes.string,
   className: PropTypes.string,
-};
-
-Timeline.defaultProps = {
-  className: '',
 };
 
 export default Timeline;
