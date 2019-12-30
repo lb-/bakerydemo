251443f8ead98b476a43c8ebde913d4b841a6056
LB <mail@lb.ee>
2019-12-30 11:17:01 +1000

    answer question (basic)

--- bakerydemo/base/wagtail_hooks.py
+++ bakerydemo/base/wagtail_hooks.py
@@ -1,66 +1,59 @@
+from wagtail.admin.menu import MenuItem
+
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin, ModelAdminGroup, modeladmin_register)
 
-from bakerydemo.breads.models import Country, BreadIngredient, BreadType
-from bakerydemo.base.models import People, FooterText
+from bakerydemo.blog.models import BlogPage
 
-'''
-N.B. To see what icons are available for use in Wagtail menus and StreamField block types,
-enable the styleguide in settings:
+class ModelAdminQueryMenuItem(MenuItem):
 
-INSTALLED_APPS = (
-   ...
-   'wagtail.contrib.styleguide',
-   ...
-)
+    def __init__(self, model_admin, order, query, label_append=''):
+        self.model_admin = model_admin
+        url = model_admin.url_helper.index_url + "?" + query
+        classnames = 'icon icon-%s' % model_admin.get_menu_icon()
+        super().__init__(
+            label=model_admin.get_menu_label() + label_append,
+            url=url,
+            classnames=classnames,
+            order=order
+        )
 
-or see http://kave.github.io/general/2015/12/06/wagtail-streamfield-icons.html
-
-This demo project includes the full font-awesome set via CDN in base.html, so the entire
-font-awesome icon set is available to you. Options are at http://fontawesome.io/icons/.
-'''
-
-
-class BreadIngredientAdmin(ModelAdmin):
-    # These stub classes allow us to put various models into the custom "Wagtail Bakery" menu item
-    # rather than under the default Snippets section.
-    model = BreadIngredient
+    def is_shown(self, request):
+        return self.model_admin.permission_helper.user_can_list(request.user)
 
 
-class BreadTypeAdmin(ModelAdmin):
-    model = BreadType
+class BlogPageAdmin(ModelAdmin):
+    model = BlogPage
 
+    def get_menu_items(self, order=None):
+        # new class method that builds a list of menu_item(s)
+        menu_items = []
 
-class BreadCountryAdmin(ModelAdmin):
-    model = Country
+        ## build 'live only' (no unpublished changes) items
+        live_menu_item = ModelAdminQueryMenuItem(self, order or self.get_menu_order(), query='has_unpublished_changes=False', label_append=' (Live)')
+        menu_items.append(live_menu_item)
 
+        ## build 'draft' items
+        draft_menu_item = ModelAdminQueryMenuItem(self, order or self.get_menu_order(), query='has_unpublished_changes=True', label_append=' (Draft)')
+        menu_items.append(draft_menu_item)
 
-class BreadModelAdminGroup(ModelAdminGroup):
-    menu_label = 'Bread Categories'
-    menu_icon = 'fa-suitcase'  # change as required
-    menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
-    items = (BreadIngredientAdmin, BreadTypeAdmin, BreadCountryAdmin)
+        return menu_items
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        ## read the request and modify the qs as needed if query param does not work easily
+        return qs
 
-class PeopleModelAdmin(ModelAdmin):
-    model = People
-    menu_label = 'People'  # ditch this to use verbose_name_plural from model
-    menu_icon = 'fa-users'  # change as required
-    list_display = ('first_name', 'last_name', 'job_title', 'thumb_image')
+class BlogGroup(ModelAdminGroup):
+    menu_label = 'Blog'
+    items = (BlogPageAdmin, )
 
+    def get_submenu_items(self):
+        menu_items = []
+        item_order = 1
+        for modeladmin in self.modeladmin_instances:
+            menu_items = menu_items + modeladmin.get_menu_items(item_order)
+            item_order = len(menu_items) + 1
+        return menu_items
 
-class FooterTextAdmin(ModelAdmin):
-    model = FooterText
-
-
-class BakeryModelAdminGroup(ModelAdminGroup):
-    menu_label = 'Bakery Misc'
-    menu_icon = 'fa-cutlery'  # change as required
-    menu_order = 300  # will put in 4th place (000 being 1st, 100 2nd)
-    items = (PeopleModelAdmin, FooterTextAdmin)
-
-
-# When using a ModelAdminGroup class to group several ModelAdmin classes together,
-# you only need to register the ModelAdminGroup class with Wagtail:
-modeladmin_register(BreadModelAdminGroup)
-modeladmin_register(BakeryModelAdminGroup)
+modeladmin_register(BlogGroup)
