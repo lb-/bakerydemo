93e6d2005419b56f2190f9c9f95344cde84f2e06
LB <mail@lb.ee>
2020-01-19 10:25:19 +1000

    wagtail hooks - add button & basic JS handling on button

--- bakerydemo/base/wagtail_hooks.py
+++ bakerydemo/base/wagtail_hooks.py
@@ -1,5 +1,9 @@
+from django.utils.html import format_html
+
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin, ModelAdminGroup, modeladmin_register)
+from wagtail.admin.widgets import PageListingButton
+from wagtail.core import hooks
 
 from bakerydemo.breads.models import Country, BreadIngredient, BreadType
 from bakerydemo.base.models import People, FooterText
@@ -59,6 +63,41 @@ class BakeryModelAdminGroup(ModelAdminGroup):
     menu_order = 300  # will put in 4th place (000 being 1st, 100 2nd)
     items = (PeopleModelAdmin, FooterTextAdmin)
 
+@hooks.register('register_page_listing_buttons')
+def page_listing_buttons(page, page_perms, is_parent=False):
+
+    attrs = {
+        'data-id': page.pk, # note - may want to html encode this for a more secure implementation
+    }
+
+    yield PageListingButton(
+        'Regenerate Geo Features',
+        '#',
+        attrs=attrs,
+        classes=["action-regenerate-geo-features"],
+        priority=100
+    )
+
+@hooks.register('insert_global_admin_js')
+def global_admin_js():
+    return format_html(
+        """
+        <script>
+        const onClickHandler = function(event) {{
+            event.preventDefault(); // ensure the hash does not change
+            console.log('clicked', event.target.dataset.id);
+        }};
+
+        window.addEventListener('DOMContentLoaded', function(event) {{
+            const actionButtons = Array.from(document.getElementsByClassName('action-regenerate-geo-features'));
+
+            actionButtons.forEach(function(element) {{
+                element.addEventListener('click', onClickHandler);
+            }});
+        }});
+        </script>
+        """,
+    )
 
 # When using a ModelAdminGroup class to group several ModelAdmin classes together,
 # you only need to register the ModelAdminGroup class with Wagtail:
