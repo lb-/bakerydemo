954cc9124a778a85377b94ab9be281f95df3cebf
LB <mail@lb.ee>
2020-05-07 18:34:30 +1000

    Revert "cleanup"
    This reverts commit e813729d5c5f2e964d41279ef7fbdca9237fbd22.

--- bakerydemo/base/templatetags/modal_tags.py
+++ bakerydemo/base/templatetags/modal_tags.py
@@ -1,22 +1,22 @@
-# from django import template
-# from bakerydemo.base.models import MyAppSettings
+from django import template
+from bakerydemo.base.models import MyAppSettings
 
-# register = template.Library()
+register = template.Library()
 
-# # reminder - you will need to restart Django when adding a template tag
+# reminder - you will need to restart Django when adding a template tag
 
 
-# @register.inclusion_tag('tags/form_modal.html', takes_context=True)
-# def form_modal(context):
-#     request = context['request']  # important - you must have the request in context
-#     settings = MyAppSettings.for_request(request)
-#     form_page = settings.modal_form_page.specific
+@register.inclusion_tag('tags/form_modal.html', takes_context=True)
+def form_modal(context):
+    request = context['request']  # important - you must have the request in context
+    settings = MyAppSettings.for_request(request)
+    form_page = settings.modal_form_page.specific
 
-#     # this will provide the parts needed to render the form
-#     # this does NOT handle the submission of the form - that still goes to the form page
-#     # this does NOT handle anything to do with rendering the 'thank you' message
+    # this will provide the parts needed to render the form
+    # this does NOT handle the submission of the form - that still goes to the form page
+    # this does NOT handle anything to do with rendering the 'thank you' message
 
-#     context['page'] = form_page
-#     context['form'] = form_page.get_form(page=form_page, user=request.user)
+    context['page'] = form_page
+    context['form'] = form_page.get_form(page=form_page, user=request.user)
 
-#     return context
+    return context
