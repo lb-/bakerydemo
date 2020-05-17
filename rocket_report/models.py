58dd0516c83894125aef831f6136be8ed10bf817
LB <mail@lb.ee>
2020-05-17 12:48:59 +1000

    1c - Prepare a ModelAdmin model - create snippet

--- rocket_report/models.py
+++ rocket_report/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from modelcluster.fields import ParentalKey
 
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
@@ -26,7 +28,7 @@ class RocketReportPage(Page):
         FieldPanel("byline"),
         FieldPanel("preamble", classname="full"),
         ImageChooserPanel("main_image"),
-        # TBC - reports
+        InlinePanel("rocket_reports", label="Rocket Reports"),
         InlinePanel("related_launches", label="Timeline"),
     ]
 
@@ -42,3 +44,59 @@ class Launch(Orderable):
         FieldPanel("date"),
         FieldPanel("details"),
     ]
+
+
+@register_snippet
+class RocketReport(models.Model):
+
+    STATUS_CHOICES = [
+        ("SUBMITTED", "Submitted"),
+        ("REVIEWED", "Reviewed"),
+        ("PROPOSED", "Proposed"),
+        ("HOLD", "Hold"),
+        ("CURRENT", "Current"),
+    ]
+
+    CATEGORY_CHOICES = [
+        ("BLANK", "Uncategorised"),
+        ("SMALL", "Small"),
+        ("MEDIUM", "Medium"),
+        ("LARGE", "Large"),
+    ]
+
+    submitted_url = models.URLField(null=True, blank=True)
+    submitted_by = models.CharField(max_length=255, blank=True)
+    status = models.CharField(max_length=255, blank=True, choices=STATUS_CHOICES)
+    title = models.CharField(max_length=255)
+    content = RichTextField(blank=True)
+    category = models.CharField(
+        max_length=255, choices=CATEGORY_CHOICES, default="BLANK"
+    )
+
+    panels = [
+        FieldPanel("title"),
+        FieldPanel("status"),
+        FieldPanel("category"),
+        FieldPanel("content"),
+        FieldPanel("submitted_url"),
+        FieldPanel("submitted_by"),
+    ]
+
+    def __str__(self):
+        return self.title
+
+
+class RocketReportPageReportPlacement(Orderable, models.Model):
+    page = ParentalKey(
+        RocketReportPage, on_delete=models.CASCADE, related_name="rocket_reports"
+    )
+    rocket_report = models.ForeignKey(
+        RocketReport, on_delete=models.CASCADE, related_name="+"
+    )
+
+    panels = [
+        SnippetChooserPanel("rocket_report"),
+    ]
+
+    def __str__(self):
+        return self.page.title + " -> " + self.rocket_report.title
