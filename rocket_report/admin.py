28ad99dfa67dc789df16ed4c529b13bf2e38d573
LB <mail@lb.ee>
2020-05-17 15:56:24 +1000

    2b - Create a template, view & mixin - Create Mixin with template override

--- rocket_report/admin.py
+++ rocket_report/admin.py
@@ -3,7 +3,13 @@ from wagtail.contrib.modeladmin.options import ModelAdmin
 from .models import RocketReport
 
 
-class RocketReportAdmin(ModelAdmin):
+class KanbanMixin:
+    def get_index_template(self):
+        # leverage the get_template to allow individual override on a per model basis
+        return self.index_template_name or self.get_templates("kanban_index")
+
+
+class RocketReportAdmin(KanbanMixin, ModelAdmin):
     model = RocketReport
     menu_icon = "fa-rocket"
     list_display = ("title", "status", "category", "submitted_by")
