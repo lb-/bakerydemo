3714cbc30b7f33658802ba3acfb3217e8c3a7c70
LB <mail@lb.ee>
2020-05-23 14:33:17 +1000

    CSS approach

--- bakerydemo/base/wagtail_hooks.py
+++ bakerydemo/base/wagtail_hooks.py
@@ -46,10 +46,11 @@ class BreadModelAdminGroup(ModelAdminGroup):
 
 
 class PeopleModelAdmin(ModelAdmin):
+    index_view_extra_css = ('css/modeladmin-index.css',)
     model = People
     menu_label = 'People'  # ditch this to use verbose_name_plural from model
     menu_icon = 'fa-users'  # change as required
-    list_display = ('first_name', 'last_name', 'job_title', 'thumb_image')
+    list_display = ('first_name', 'last_name', 'job_title', 'thumb_image', 'first_name', 'last_name', 'job_title', 'thumb_image', 'first_name', 'last_name', 'job_title', 'thumb_image')
     list_filter = ('job_title', )
     search_fields = ('first_name', 'last_name', 'job_title')
 
