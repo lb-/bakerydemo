fb19d849ce899d36e1ae3451b11ce9a35195d0d1
LB <mail@lb.ee>
2020-09-23 06:29:32 +1000

    Step 6 - Process the Image (file) Data after Validation

--- bakerydemo/base/models.py
+++ bakerydemo/base/models.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals
 
+import json
+from os.path import splitext
+
+from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 
 from modelcluster.fields import ParentalKey
@@ -17,6 +21,7 @@ from wagtail.core.fields import RichTextField, StreamField
 from wagtail.core.models import Collection, Page
 from wagtail.contrib.forms.forms import FormBuilder
 from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField, FORM_FIELD_CHOICES
+from wagtail.images import get_image_model
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.images.fields import WagtailImageField
 from wagtail.search import index
@@ -407,3 +412,60 @@ class FormPage(AbstractEmailForm):
         collection = self.uploaded_image_collection
 
         return collection or Collection.get_first_root_node()
+
+    @staticmethod
+    def get_image_title(filename):
+        """
+        Generates a usable title from the filename of an image upload.
+        Note: The filename will be provided as a 'path/to/file.jpg'
+        """
+
+        if filename:
+            result = splitext(filename)[0]
+            result = result.replace('-', ' ').replace('_', ' ')
+            return result.title()
+        return ''
+
+    def process_form_submission(self, form):
+        """
+        Processes the form submission, if an Image upload is found, pull out the
+        files data, create an actual Wgtail Image and reference its ID only in the
+        stored form response.
+        """
+
+        cleaned_data = form.cleaned_data
+
+        for name, field in form.fields.items():
+            if isinstance(field, WagtailImageField):
+                image_file_data = cleaned_data[name]
+                if image_file_data:
+                    ImageModel = get_image_model()
+
+                    kwargs = {
+                        'file': cleaned_data[name],
+                        'title': self.get_image_title(cleaned_data[name].name),
+                        'collection': self.get_uploaded_image_collection(),
+                    }
+
+                    if form.user and not form.user.is_anonymous:
+                        kwargs['uploaded_by_user'] = form.user
+
+                    image = ImageModel(**kwargs)
+                    image.save()
+                    # saving the image id
+                    # alternatively we can store a path to the image via image.get_rendition
+                    cleaned_data.update({name: image.pk})
+                else:
+                    # remove the value from the data
+                    del cleaned_data[name]
+
+        submission = self.get_submission_class().objects.create(
+            form_data=json.dumps(form.cleaned_data, cls=DjangoJSONEncoder),
+            page=self,
+        )
+
+        # important: if extending AbstractEmailForm, email logic must be re-added here
+        # if self.to_address:
+        #    self.send_mail(form)
+
+        return submission
