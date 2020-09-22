4c49f8b48e01dd39eff7fbce9019bb73e82b1964
LB <mail@lb.ee>
2020-09-23 07:33:57 +1000

    Step 7 - Viewing the image via the form submissions listing

--- bakerydemo/base/models.py
+++ bakerydemo/base/models.py
@@ -5,6 +5,8 @@ from os.path import splitext
 
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
+from django.utils.html import format_html
+from django.urls import reverse
 
 from modelcluster.fields import ParentalKey
 from modelcluster.models import ClusterableModel
@@ -21,6 +23,7 @@ from wagtail.core.fields import RichTextField, StreamField
 from wagtail.core.models import Collection, Page
 from wagtail.contrib.forms.forms import FormBuilder
 from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField, FORM_FIELD_CHOICES
+from wagtail.contrib.forms.views import SubmissionsListView
 from wagtail.images import get_image_model
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.images.fields import WagtailImageField
@@ -344,6 +347,41 @@ class CustomFormBuilder(FormBuilder):
         return WagtailImageField(**options)
 
 
+class CustomSubmissionsListView(SubmissionsListView):
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if not self.is_export:
+            # generate a list of field types, the first being the injected 'submission date'
+            field_types = ['submission_date'] + [field.field_type for field in self.form_page.get_form_fields()]
+            data_rows = context['data_rows']
+
+            ImageModel = get_image_model()
+
+            for data_row in data_rows:
+
+                fields = data_row['fields']
+
+                for idx, (value, field_type) in enumerate(zip(fields, field_types)):
+                    if field_type == 'image' and value:
+                        image = ImageModel.objects.get(pk=value)
+                        rendition = image.get_rendition('fill-100x75|jpegquality-40')
+                        preview_url = rendition.url
+                        url = reverse('wagtailimages:edit', args=(image.id,))
+                        # build up a link to the image, using the image title & id
+                        fields[idx] = format_html(
+                            "<a href='{}'><img alt='Uploaded image - {}' src='{}' />{} ({})</a>",
+                            url,
+                            image.title,
+                            preview_url,
+                            image.title,
+                            value
+                        )
+
+        return context
+
+
 class FormField(AbstractFormField):
     """
     Wagtailforms is a module to introduce simple forms on a Wagtail site. It
@@ -366,6 +404,7 @@ class FormField(AbstractFormField):
 class FormPage(AbstractEmailForm):
 
     form_builder = CustomFormBuilder
+    submissions_list_view_class = CustomSubmissionsListView
 
     image = models.ForeignKey(
         'wagtailimages.Image',
