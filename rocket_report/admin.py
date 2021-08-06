b8bcf651e3e01730b00db8dba0e6e27df4384ae1
LB Johnston <lb.johnston@virginaustralia.com>
2021-08-06 20:59:03 +1000

    3a - Pull in actual data - revise the KanbanMixin and add small templates

--- rocket_report/admin.py
+++ rocket_report/admin.py
@@ -12,6 +12,32 @@ class KanbanMixin:
         return self.index_template_name or self.get_templates("kanban_index")
 
 
+    def get_kanban_item_template(self):
+        # leverage the get_template to allow individual override on a per model basis
+        return getattr(
+            self, "kanban_item_template_name", self.get_templates("kanban_item")
+        )
+
+    def get_kanban_column_title_template(self):
+        # leverage the get_template to allow individual override on a per model basis
+        return getattr(
+            self,
+            "kanban_column_title_template_name",
+            self.get_templates("kanban_column_title"),
+        )
+
+    def get_kanban_column_field(self):
+        # return a field to use to determine which column the item will be shown in
+        # pull in the first value from list_filter if no specific column set
+        list_filter = getattr(self, "list_filter", [])
+        field = list_filter[0] if list_filter else None
+        return field
+
+    def get_kanban_column_name_default(self):
+        # used for the column title name for None or no column scenarios
+        return getattr(self, "kanban_column_name_default", "Other")
+
+
 class RocketReportAdmin(KanbanMixin, ModelAdmin):
     model = RocketReport
     menu_icon = "fa-rocket"
