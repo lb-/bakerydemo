b37e22be5e1415a6e0a3f7b09e92bc1d8c6bd87b
LB Johnston <lb.johnston@virginaustralia.com>
2021-08-12 13:05:45 +1000

    2 - add models

--- guide/models.py
+++ guide/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+
+from wagtail.core.models import Orderable
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+
+
+class GuideStep(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
+    element = models.CharField(max_length=255, blank=True)
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('text'),
+        FieldPanel('element'),
+    ]
+
+    class Meta:
+        abstract = True
+
+class GuideRelatedStep(Orderable, GuideStep):
+    guide = ParentalKey('guide.Guide', on_delete=models.CASCADE, related_name='steps')
+
+
+class Guide(ClusterableModel):
+    title = models.CharField(max_length=255)
+    # steps - see GuideRelatedStep
+    url_path = models.CharField(max_length=255, blank=True)
+
+    panels = [
+        FieldPanel('title'),
+        InlinePanel('steps', label="Steps", min_num=1),
+        FieldPanel('url_path'),
+    ]
