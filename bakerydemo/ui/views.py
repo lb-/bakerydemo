54cdf4fd2bfce7807057c4d893442feb9447b32f
LB Johnston <lb.johnston@virginaustralia.com>
2022-01-21 18:47:27 +1000

    basic functional collapse

--- bakerydemo/ui/views.py
+++ bakerydemo/ui/views.py
@@ -4,5 +4,8 @@ class StimulusPanel(Component):
     order = 10
     template_name = 'ui/panels/stimulus.html'
 
+    # class Media:
+    #     js = ('https://unpkg.com/@hotwired/stimulus/dist/stimulus.js',)
+
 
 my_welcome_panel = StimulusPanel()
