5871046370be90084b9bc4b3a9e4eaeb21d843f6
LB Johnston <lb.johnston@virginaustralia.com>
2022-01-24 08:43:01 +1000

    refine demo to emulate a wagtail global
    - add util getControllerByIdentifier to pull out an existing registered controller
    - move canonical 'hello' to the core registered application for the demo

--- bakerydemo/ui/wagtail_hooks.py
+++ bakerydemo/ui/wagtail_hooks.py
@@ -15,10 +15,32 @@ def add_stimulus_module():
     """
     <script type="module">
       import { Application, Controller } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js";
-      window.Stimulus = Application.start();
-      window.Controller = Controller;
+      window.wagtail.stimulus = {};
+      window.wagtail.stimulus.Application = Application.start();
+      window.wagtail.stimulus.Controller = Controller;
+      // note: this may not be the best API to use, just explored the available attributes.
+      // the intent is that this is similar to getControllerByNameAndIdentifier but without the name part
+      window.wagtail.stimulus.getControllerByIdentifier = (identifier) => window.wagtail.stimulus.Application.router.modulesByIdentifier.get(identifier).definition.controllerConstructor;
 
       // future - base Wagtail controllers would be registered first
+
+      /** Basic Hello controller to demo a Wagtail introduced controller
+       * that can be pulled in, extended and override existing behaviour object oriented style.
+       */
+      class Hello extends Controller {
+        static targets = [ "name" ];
+        static values = { label: { default: '', type: String }, replace: { default: false, type: Boolean } };
+
+        doLog() {
+          console.log('hello stimulus', this.element);
+        }
+
+        connect() {
+          this.doLog(name);
+        }
+      }
+
+      window.wagtail.stimulus.Application.register('hello', Hello);
 
       // future - Stimulus.debug = true/false (based on env)
 
