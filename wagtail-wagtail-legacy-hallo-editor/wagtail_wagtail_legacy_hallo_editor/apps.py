2e358c0e9e2ead716eb9ecd32564b5a70f7a575e
LB Johnston <lb.johnston@virginaustralia.com>
2022-01-26 20:41:05 +1000

    output from wagtail project cookiecutter

---
+++ wagtail-wagtail-legacy-hallo-editor/wagtail_wagtail_legacy_hallo_editor/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class WagtailWagtailLegacyHalloEditorAppConfig(AppConfig):
+    label = "wagtail_wagtail_legacy_hallo_editor"
+    name = "wagtail_wagtail_legacy_hallo_editor"
+    verbose_name = "Wagtail wagtail-legacy-hallo-editor"
