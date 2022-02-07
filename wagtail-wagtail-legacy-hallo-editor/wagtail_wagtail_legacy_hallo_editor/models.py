b647f6f2706e23fded9353b2dc17b888d94a3d53
LB Johnston <lb.johnston@virginaustralia.com>
2022-02-08 08:06:19 +1000

    remove package contents from this branch
    - now on https://github.com/lb-/wagtail-hallo

--- wagtail-wagtail-legacy-hallo-editor/wagtail_wagtail_legacy_hallo_editor/models.py
+++
@@ -1,9 +0,0 @@
-from wagtail.admin.edit_handlers import FieldPanel
-from wagtail.core.fields import RichTextField
-from wagtail.core.models import Page
-
-class HalloTestPage(Page):
-    body = RichTextField(editor='legacy', blank=True)
-    content_panels = Page.content_panels + [
-        FieldPanel('body', classname='full'),
-    ]
