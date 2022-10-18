1fc0287385d2c373cef2edfc4303004b763a1a8a
LB Johnston <mail@lb.ee>
2022-10-19 07:43:22 +1000

    get something basic functional

--- bakerydemo/process/blocks.py
+++ bakerydemo/process/blocks.py
@@ -1,33 +1,102 @@
 from wagtail.blocks import (
     CharBlock,
     StreamBlock,
+    ListBlock,
     StructBlock,
     TextBlock,
 )
 
-class EndBlock(StructBlock):
+from wagtail.documents.blocks import DocumentChooserBlock
+
+
+class BaseStepBlock(StructBlock):
+    label = CharBlock()
+    description = TextBlock(blank=True, required=False)
+
+    class Meta:
+        icon = "placeholder"
+
+class DocumentBlock(BaseStepBlock):
+
+    document = DocumentChooserBlock()
+
+    class Meta:
+        icon = "doc-empty"
+        label = "Document"
+
+class EndBlock(BaseStepBlock):
     """
     Custom `StructBlock` that represents the end of a process.
     """
 
-    label = CharBlock()
-    description = TextBlock(blank=True, required=False)
+    class Meta:
+        form_classname="end-block-wrapper"
+        icon = "radio-full"
 
 
-class StepBlock(StructBlock):
+class EventBlock(BaseStepBlock):
     """
-    Custom `StructBlock` that represents a step.
+    Custom `StructBlock` that represents an external event.
     """
 
-    label = CharBlock()
-    description = TextBlock(blank=True, required=False)
+    class Meta:
+        icon = "media"
+        label = "Event"
 
 
-# StreamBlocks
-class StepsStreamBlock(StreamBlock):
+class StartBlock(BaseStepBlock):
+    """
+    Custom `StructBlock` that represents how a process starts.
+    """
+
+    class Meta:
+        icon = "radio-empty"
+        label ="Start"
+
+
+class TaskBlock(BaseStepBlock):
+    """
+    Custom `StructBlock` that represents an activity (task) to perform.
+    """
+
+    class Meta:
+        icon = "tick"
+        label ="Task"
+
+
+class BaseStepsStreamBlock(StreamBlock):
+
+    # general activity blocks
+    task_block = TaskBlock()
+    event_block = EventBlock()
+    document_block = DocumentBlock()
+
+    # special end block (should not be able to add steps after end in a Stream)
+    end_block = EndBlock()
+
+class ExclusiveGatewayBlock(BaseStepsStreamBlock):
+    class Meta:
+        icon = "pick"
+        label = "Decision"
+
+
+
+class StartStreamBlock(StreamBlock):
+    start_block = StartBlock()
+
+    class Meta:
+        block_counts = {'start_block': {'min_num': 1, 'max_num': 1}}
+        label = "Start"
+
+
+class StepsStreamBlock(BaseStepsStreamBlock):
     """
     Define the custom blocks that `StreamField` will utilize
+    Should have a list of generic StepBlocks
+    A step block can be an activity, end, gateway (OR), event, document
+    Also -each thing can have a lane (later)
     """
 
-    step_block = StepBlock()
-    end_block = EndBlock()
+    # add ability for nested sets of process steps
+    exclusive_gateway_block = ExclusiveGatewayBlock()
+
--- bakerydemo/process/models.py
+++ bakerydemo/process/models.py
@@ -6,31 +6,12 @@ from wagtail.admin.panels import FieldPanel, InlinePanel
 from modelcluster.fields import ParentalKey
 from modelcluster.models import ClusterableModel
 
-from .blocks import StepsStreamBlock
+from .blocks import StartStreamBlock, StepsStreamBlock
 
-
-
-class ProcessStart(models.Model):
-    """
-    Store the way a process starts as a Django model.
-    """
-
-    process = ParentalKey(
-        "Process",
-        related_name="start",
-        null=True,
-        on_delete=models.SET_NULL
-    )
-
-    title = models.CharField(blank=True, max_length=255)
-
-    panels = [
-        FieldPanel("title")
-    ]
 
 
 @register_snippet
-class Process(ClusterableModel):
+class Process(models.Model):
     """
     Stores a business process with the goal of aligning with BPMN basics.
     """
@@ -39,6 +20,13 @@ class Process(ClusterableModel):
     description = models.TextField(
         help_text="Description of this process",
         blank=True,
+    )
+    start = StreamField(
+        StartStreamBlock,
+        verbose_name="Start",
+        help_text="How does this process start?",
+        blank=True,
+        use_json_field=True,
     )
     steps = StreamField(
         StepsStreamBlock(),
@@ -51,7 +39,7 @@ class Process(ClusterableModel):
     panels = [
         FieldPanel("title"),
         FieldPanel("description"),
-        InlinePanel("start", min_num=1, max_num=1),
+        FieldPanel("start"),
         FieldPanel("steps"),
     ]
 
