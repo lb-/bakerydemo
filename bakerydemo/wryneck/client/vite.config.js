99c81f6f3c00e05f6c5a9235e7de16d1a19ee975
LB <mail@lb.ee>
2023-04-23 15:13:38 +0800

    Add initial vite config

---
+++ bakerydemo/wryneck/client/vite.config.js
@@ -0,0 +1,30 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    emptyOutDir: true,
+    outDir: '../static/wryneck/',
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'lib/main.js'),
+      formats: ['iife'],
+      name: 'Wryneck',
+      // the proper extensions will be added
+      fileName: 'wryneck',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['@hotwired/stimulus'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          "@hotwired/stimulus": 'window.Stimulus.constructor',
+        },
+      },
+    },
+  },
+})
