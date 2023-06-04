12f3353ec9e37696787485a1dc89784e328a13d5
LB <mail@lb.ee>
2023-06-04 12:25:15 +1000

    Add starting wryneck stuff

--- bakerydemo/wryneck/client/WryneckController.js
+++ bakerydemo/wryneck/client/WryneckController.js
@@ -3,7 +3,9 @@ import * as stimulus from "@hotwired/stimulus";
 const { Controller } = stimulus;
 
 export class WryneckController extends Controller {
+  static targets = ['input'];
+
   connect() {
-    console.log('wryneck connected!');
+    console.log('wryneck connected!', this.inputTarget);
   }
 }
--- bakerydemo/wryneck/widgets.py
+++ bakerydemo/wryneck/widgets.py
@@ -3,7 +3,7 @@ from django.forms import Media, widgets
 from django.conf.urls.static import static
 
 
-class WryneckRichTextArea(widgets.TextInput):
+class WryneckRichTextArea(widgets.HiddenInput):
     # template_name = "widgets/wryneck_rich_text_area.html"
     is_hidden = False
 
@@ -14,20 +14,22 @@ class WryneckRichTextArea(widgets.TextInput):
     show_add_comment_button = True
 
     def __init__(self, *args, **kwargs):
-        print('init??')
         self.features = kwargs.pop("features", None)
         kwargs.pop("options", None)
 
         self.options = {}
         self.plugins = []
 
-        default_attrs = {"data-controller": "wryneck"}
+        default_attrs = {"data-wryneck-target": "input"}
         attrs = kwargs.get("attrs")
         if attrs:
             default_attrs.update(attrs)
         kwargs["attrs"] = default_attrs
 
         super().__init__(*args, **kwargs)
+
+        # find a nicer way to do this
+        self.template_name = 'widgets/wryneck_rich_text.html'
 
     @property
     def media(self):
